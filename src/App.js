6b02c7887d1386ec720aaac7e88c345a847eb5a5
Daniela Aldaco <danielaaldaco9@gmail.com>
2025-12-09 12:00:46 -0600

    Login y registro terminado, con archivo style.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Header from "./app/components/Header";
 import Login from "./app/components/Login";
 import Registro from "./app/components/Registro";
 import Dashboard from "./app/components/Dashboard";
 
-
 function App() {
   return (
     <Router>
-      <Header />
+      <MainRoutes />
+    </Router>
+  );
+}
+
+function MainRoutes() {
+  const location = useLocation();
+
+  const hideHeaderRoutes = ["/login", "/registro"];
+
+  const hideHeader = hideHeaderRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!hideHeader && <Header />}
 
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.css"; // Asume que tienes un index.css
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import "react-toastify/dist/ReactToastify.css";
+import "./style.css"; // <<-- RUTA CORRECTA (Si index.js y style.css están en /src)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
